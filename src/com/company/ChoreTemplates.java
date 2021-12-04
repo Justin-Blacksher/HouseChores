d309aa6d82f235dc306bd71a61fb91cae1b3e2ad
Justin Blacksher <92361451+Justin-Blacksher@users.noreply.github.com>
2021-12-03 19:30:30 -0700

    Beginning of school project for my group.
    Tiffany Blacksher
    Justin Blacksher

--- src/com/company/ChoreTemplates.java
+++ src/com/company/ChoreTemplates.java
@@ -4,13 +4,36 @@ public class ChoreTemplates {
 
     // TODO: Create Templates for use in creation of chore charts
     static String[] Sunday = {"Clean Shower", "Clean Toilet", "Mop Hallway", "Sweep Porch", "Read 30 Minutes",
-            "Shower", "Lay out clothes for school", "Organize schoolwork"
-    };
-
+            "Shower", "Lay out clothes for school", "Organize schoolwork"};
     static String[] BathroomChart = {"Clean toilet", "Clean Shower", "Mop Floor", "Clean Sink"};
-    static String[] KitchenChart = {};
-    static String[] HallwayChart = {};
-    static String[] BedroomChart = {};
-    static String[] EducationChart = {};
+    static String[] KitchenChart = {"Dishes", "Clean Fridge", "Mop floor", "Clean Microwave"};
+    static String[] HallwayChart = {"Sweep Floor", "Shake Rugs", "Mop Floor", "Clean Molding ledges"};
+    static String[] BedroomChart = {"Make Bed", "Clean under bed", "Organize closet","Organize bookshelf", "Clean walls"};
+    static String[] EducationChart = {"Check canvas", "Make study todo list", "Do all late work", "Read 30m", "Study 20m"};
+
+    public static String[] getSunday() {
+        return Sunday;
+    }
+
+    public static String[] getBathroomChart() {
+        return BathroomChart;
+    }
+
+    public static String[] getKitchenChart() {
+        return KitchenChart;
+    }
+
+    public static String[] getHallwayChart() {
+        return HallwayChart;
+    }
+
+    public static String[] getBedroomChart() {
+        return BedroomChart;
+    }
+
+    public static String[] getEducationChart() {
+        return EducationChart;
+    }
+
 
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     private static String[] args;
 
     public static void main(String[] args) {
 
-
+        // TODO: MAKE TEST FUNCTIONS TO TEST FUNCTIONALITY OF ChoreTemplates
         // TODO: Test the functionality of the Default constructor
         ChoreChart chores = new ChoreChart();
         System.out.println(chores.getChartName());
@@ -28,12 +30,33 @@ public class Main {
         // TODO: Test functionality of the template chore charts
 
         System.out.println("---------------------------------------------------------------------------");
-        ChoreChart SundayChores = new ChoreChart(ChoreTemplates.Bathroom, "Sunday Chores");
-        System.out.println(SundayChores.getChartName());
-        System.out.println(SundayChores.getChoreStatus());
-        System.out.println(SundayChores.getChoreCount());
-        System.out.println(SundayChores.getChoreList());
+        ChoreChart Bathroom = new ChoreChart(ChoreTemplates.BathroomChart, "Bathroom Chores");
+        System.out.println(Bathroom.getChartName());
+        System.out.println(Bathroom.getChoreStatus());
+        System.out.println(Bathroom.getChoreCount());
+        System.out.println(Bathroom.getChoreList());
 
+        // TODO: Test functionality of the template chore charts
+
+        System.out.println("---------------------------------------------------------------------------");
+        ChoreChart Kitchen = new ChoreChart(ChoreTemplates.KitchenChart, "Kitchen Chores");
+        System.out.println(Kitchen.getChartName());
+        System.out.println(Kitchen.getChoreStatus());
+        System.out.println(Kitchen.getChoreCount());
+        System.out.println(Kitchen.getChoreList());
+
+        System.out.println("-----------------------------------------------------------------------------");
+
+        // --------------------------------------------- MAIN FUNCIONS ----------------------------------
+        // TODO Write Main functions specific to Main
+        // TODO: Create Function that grabs all available chore charts
+        /*public String viewTemplates(){
+            System.out.println("Bathroom Chores:");
+            System.out.println("------------------")
+            System.out.println(Arrays.toString(ChoreTemplates.getBathroomChart()));
+
+               }
+        */    
 
     }
 }
