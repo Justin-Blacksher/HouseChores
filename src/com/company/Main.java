f4b8e40e17f4676c93ceb5afc0ad23837cc0db30
Justin Blacksher <92361451+Justin-Blacksher@users.noreply.github.com>
2021-12-03 20:32:35 -0700

    Beginning of school project for my group.
    Tiffany Blacksher
    Justin Blacksher

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,51 @@
 /**
+ * <h1>Create a chore chart!</h1>
+ * <h2>This file determines the route to take</h2>
+ * <p>Note: This program has not been made to be non-volatile as of yet
+ * Care must be taken not to shut the program down if you are intending on
+ * actually using it.</p>
  * @author: Tiffany Blacksher
  * @author: Justin Blacksher
  * @Filename: Main.java
  * @description: Main file that will control the program as a whole.
  */
 package com.company;
-
+import java.util.Scanner;
 import java.util.Arrays;
+
 
 public class Main {
 
     private static String[] args;
 
+    // TODO Write Main functions specific to Main
+    // TODO: Create Function that grabs all available chore charts
+
+    /*
+
+    public String viewTemplates(){
+        System.out.println("Bathroom Chores:");
+        System.out.println("------------------")
+        System.out.println(Arrays.toString(ChoreTemplates.getBathroomChart()));
+
+           }
+    */
+
+    public static void menuTree(){      // Decision Tree for creating the menu
+        Boolean menuItemSelected = Boolean.FALSE;
+        Boolean hasChildren = Boolean.FALSE;
+
+    }
+
+
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); // Scanner object to get user input
+
+
+
+
+
+        // ---------------------------------- TESTING SECTION -----------------------------
 
         // TODO: MAKE TEST FUNCTIONS TO TEST FUNCTIONALITY OF ChoreTemplates
         // TODO: Test the functionality of the Default constructor
@@ -53,19 +86,8 @@ public class Main {
 
         System.out.println("-----------------------------------------------------------------------------");
 
-        // --------------------------------------------- MAIN FUNCIONS ----------------------------------
-        // TODO Write Main functions specific to Main
-        // TODO: Create Function that grabs all available chore charts
 
-        /*
 
-        public String viewTemplates(){
-            System.out.println("Bathroom Chores:");
-            System.out.println("------------------")
-            System.out.println(Arrays.toString(ChoreTemplates.getBathroomChart()));
-
-               }
-        */    
 
     }
 }
