d34c87831569ec807b9c7e017f9376f33414ae0f
Justin Blacksher <92361451+Justin-Blacksher@users.noreply.github.com>
2021-12-03 23:50:52 -0700

    Beginning of school project for my group.
    Tiffany Blacksher
    Justin Blacksher

---
+++ src/com/company/Graphics.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Graphics {
+
+    public static void EntryGraphic() {
+
+
+
+       System.out.println("sCCCCCCCCCCCCChhhhhhh                                                                                   CCCCCCCCCCCCChhhhhhh                                                           tttt");
+       System.out.println("CCC::::::::::::Ch:::::h                                                                                CCC::::::::::::Ch:::::h                                                        ttt:::t");
+       System.out.println(" CC:::::::::::::::Ch:::::h                                                                              CC:::::::::::::::Ch:::::h                                                        t:::::t");
+       System.out.println(" C:::::CCCCCCCC::::Ch:::::h                                                                             C:::::CCCCCCCC::::Ch:::::h                                                        t:::::t");
+       System.out.println(" C:::::C       CCCCCC h::::h hhhhh          ooooooooooo   rrrrr   rrrrrrrrr       eeeeeeeeeeee          C:::::C       CCCCCC h::::h hhhhh         aaaaaaaaaaaaa  rrrrr   rrrrrrrrr   ttttttt:::::ttttttt");
+       System.out.println(" C:::::C               h::::hh:::::hhh     oo:::::::::::oo r::::rrr:::::::::r    ee::::::::::::ee       C:::::C               h::::hh:::::hhh      a::::::::::::a r::::rrr:::::::::r  t:::::::::::::::::t");
+       System.out.println(" C:::::C               h::::::::::::::hh  o:::::::::::::::or:::::::::::::::::r  e::::::eeeee:::::ee     C:::::C               h::::::::::::::hh    aaaaaaaaa:::::ar:::::::::::::::::r t:::::::::::::::::t");
+       System.out.println(" C:::::C               h:::::::hhh::::::h o:::::ooooo:::::orr::::::rrrrr::::::re::::::e     e:::::e     C:::::C               h:::::::hhh::::::h            a::::arr::::::rrrrr::::::rtttttt:::::::tttttt");
+       System.out.println(" C:::::C               h::::::h   h::::::ho::::o     o::::o r:::::r     r:::::re:::::::eeeee::::::e     C:::::C               h::::::h   h::::::h    aaaaaaa:::::a r:::::r     r:::::r      t:::::t");
+       System.out.println(" C:::::C               h:::::h     h:::::ho::::o     o::::o r:::::r     rrrrrrre:::::::::::::::::e      C:::::C               h:::::h     h:::::h  aa::::::::::::a r:::::r     rrrrrrr      t:::::t");
+       System.out.println(" C:::::C               h:::::h     h:::::ho::::o     o::::o r:::::r            e::::::eeeeeeeeeee       C:::::C               h:::::h     h:::::h a::::aaaa::::::a r:::::r                  t:::::t");
+       System.out.println(" C:::::C       CCCCCC h:::::h     h:::::ho::::o     o::::o r:::::r            e:::::::e                 C:::::C       CCCCCC h:::::h     h:::::ha::::a    a:::::a r:::::r                  t:::::t    tttttt");
+       System.out.println(" C:::::CCCCCCCC::::C h:::::h     h:::::ho:::::ooooo:::::o r:::::r            e::::::::e                 C:::::CCCCCCCC::::C h:::::h     h:::::ha::::a    a:::::a r:::::r                  t::::::tttt:::::t");
+       System.out.println(" CC:::::::::::::::C h:::::h     h:::::ho:::::::::::::::o r:::::r             e::::::::eeeeeeee          CC:::::::::::::::C h:::::h     h:::::ha:::::aaaa::::::a r:::::r                  tt::::::::::::::t");
+       System.out.println(" CCC::::::::::::C h:::::h     h:::::h oo:::::::::::oo  r:::::r              ee:::::::::::::e            CCC::::::::::::C h:::::h     h:::::h a::::::::::aa:::ar:::::r                    tt:::::::::::tt");
+       System.out.println(" CCCCCCCCCCCCC hhhhhhh     hhhhhhh   ooooooooooo    rrrrrrr                eeeeeeeeeeeeee               CCCCCCCCCCCCC hhhhhhh     hhhhhhh  aaaaaaaaaa  aaaarrrrrrr                      ttttttttttt");
+
+
+
+
+
+
+
+
+
+    }
+}
