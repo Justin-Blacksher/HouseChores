e02b03f9df7f1e6574f815092ae39af3cb5990e1
Justin Blacksher <92361451+Justin-Blacksher@users.noreply.github.com>
2021-12-03 23:05:23 -0700

    Beginning of school project for my group.
    Tiffany Blacksher
    Justin Blacksher

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -63,8 +63,23 @@ public class Main {
     public static void main(String[] args) {
 
 
+        // ChoreChart chart = setCustomChoreList(); Passed testing this is template for production
+        // ChoreChart SundayChores = new ChoreChart(ChoreTemplates.Sunday, "Sunday Chores"); Passed Template
+        // ChoreChart chores = new ChoreChart(); Passed for default constructor
 
+        // TODO: Create main loop to display menu and program flow
+        // TODO: Create a check and balances system to avoid user errors
+        // TODO: Use boolean check system above to achieve a "smart" feel for the program
 
+        // Menu 1: All options turned off and must initialize a chore chart and create a list of children
+        // TODO: Create menu and test
+        // TODO: (Maybe) Create MOTD and graphic to make the user feel warm and fuzzy
+        // Menu 2: Will be a modified version of Menu 1 to reflect current changes and push user into final stage
+        // TODO: Modify Menu 1 and display the instructions needed to progress into the next stage of the menu
+        // Menu 3: Full and open menu that will have full functionality and has passed its setup phase with user
+        // TODO: Finalize menu and let the user go to explore the awesome world of chores
+        // TODO: Create Visual reflecting the status of chores (Percentage complete, Jobs Available, Children, etc)
+        // TODO: Create a quick and dirty report that could be saved to a file for printing or just viewing.
 
 
 
@@ -77,8 +92,8 @@ public class Main {
 
         // TODO: MAKE TEST FUNCTIONS TO TEST FUNCTIONALITY OF ChoreTemplates
         // TODO: Test the functionality of the Default constructor
-        ChoreChart chores = new ChoreChart();
-        System.out.println(chores.getChartName());
+        ChoreChart chores = new ChoreChart();               // Testing to see if default Constructor is working
+        System.out.println(chores.getChartName());          // Can we access the default names?
         System.out.println(chores.getChoreList());
         System.out.println(chores.getChoreStatus());
         System.out.println(chores.getChoreCount());
@@ -87,8 +102,8 @@ public class Main {
         // TODO: Test functionality of the template chore charts
 
         System.out.println("---------------------------------------------------------------------------");
-        ChoreChart SundayChores = new ChoreChart(ChoreTemplates.Sunday, "Sunday Chores");
-        System.out.println(SundayChores.getChartName());
+        ChoreChart SundayChores = new ChoreChart(ChoreTemplates.Sunday, "Sunday Chores"); // Grab template
+        System.out.println(SundayChores.getChartName());    // Can we access all the predetermined values?
         System.out.println(SundayChores.getChoreStatus());
         System.out.println(SundayChores.getChoreCount());
         System.out.println(SundayChores.getChoreList());
@@ -97,8 +112,8 @@ public class Main {
         // TODO: Test functionality of the template chore charts
 
         System.out.println("---------------------------------------------------------------------------");
-        ChoreChart Bathroom = new ChoreChart(ChoreTemplates.BathroomChart, "Bathroom Chores");
-        System.out.println(Bathroom.getChartName());
+        ChoreChart Bathroom = new ChoreChart(ChoreTemplates.BathroomChart, "Bathroom Chores"); // Grab template
+        System.out.println(Bathroom.getChartName());        // Can we access all the predetermined values
         System.out.println(Bathroom.getChoreStatus());
         System.out.println(Bathroom.getChoreCount());
         System.out.println(Bathroom.getChoreList());
@@ -106,19 +121,19 @@ public class Main {
         // TODO: Test functionality of the template chore charts
 
         System.out.println("---------------------------------------------------------------------------");
-        ChoreChart Kitchen = new ChoreChart(ChoreTemplates.KitchenChart, "Kitchen Chores");
-        System.out.println(Kitchen.getChartName());
+        ChoreChart Kitchen = new ChoreChart(ChoreTemplates.KitchenChart, "Kitchen Chores"); // Grab template
+        System.out.println(Kitchen.getChartName());         // Can we access our predetermined values?
         System.out.println(Kitchen.getChoreStatus());
         System.out.println(Kitchen.getChoreCount());
         System.out.println(Kitchen.getChoreList());
 
         System.out.println("-----------------------------------------------------------------------------");
-
-        ChoreChart chart = setCustomChoreList();
-        System.out.println(chart.getChoreList());
-        chart.setChartName("Custom Chart");
-        System.out.println(chart.getChartName());
-        System.out.println(chart.getChoreCount());
+        // TODO: Test the functionality of creating a custom chore chart and displaying its information
+        ChoreChart chart = setCustomChoreList();    // Local Method to create chores and add them to chart
+        System.out.println(chart.getChoreList());   // Test to see if chore list is retrievable
+        chart.setChartName("Custom Chart");         // Can we set the new name?
+        System.out.println(chart.getChartName());   // Can we verify that the new name has in fact been changed
+        System.out.println(chart.getChoreCount());  // Will it keep count of the chores added to the list
 
 
 
